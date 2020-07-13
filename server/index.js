c2cd4f6ddbde9115be3390c5d26b0e8aa4f0ce99
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-07-13 12:23:29 +0500

    Server side finished

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const pdf = require("html-pdf");
+const cors = require("cors");
+
+const pdfTemplate = require("./documents");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post("/create-pdf", (req, res, next) => {
+  pdf.create(pdfTemplate(req.data), {}).toFile("result.pdf", (error) => {
+    if (error) return Promise.reject();
+    return Promise.resolve();
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
